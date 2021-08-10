4d93ebeff92dff040e4dc81eeee79a6121de1244
Degi100 <r.degering@mail.de>
2021-08-10 16:11:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Energy from './components/Energy';
+
+const App = () => (
+ <Energy />
+);
+export default App;
---
+++ src/components/Energy.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/energy.scss';
+
+function Energy() {
+
+	return (
+		<div className="page page_showcaseJavaScriptComponent">
+			<h2 className="title">Showcase: JavaScript Component</h2>
+			<p className="description">This is a test of a JavaScript component.</p>	
+			<p>To create a JavaScript component, create a TypeScript page first with <code>npm run dpod -- createPage --pageTitle "Test Page"</code> and then rename the component to .jsx, and then do a <code>npm start</code> again.</p>
+		</div>
+	)
+}
+
+export default Energy;
